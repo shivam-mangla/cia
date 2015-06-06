8c01f02be34dae547fa9f984ac878d2a90b7bf11
Abhishek Kandoi <abhikandoi2000@gmail.com>
2015-06-07 02:08:13 +0530

    Adds open_report_count to dashboard

--- app/controllers/PoliceDashboardController.php
+++ app/controllers/PoliceDashboardController.php
@@ -2,6 +2,7 @@
 
 use Models\PoliceMember;
 use Models\PoliceStation;
+use Models\Report;
 
 class PoliceDashboardController
 {
@@ -21,12 +22,16 @@ class PoliceDashboardController
 			header('Location: /police/login?error_msg=login_required');
 		}
 
+		$open_report_count = Report::countOpenFor($p_member);
+		// $inprocess_report_count = Report::countInprocessFor($p_member);
+
 		$p_station = PoliceStation::find(1);
 		$message = "Welcome to " . $p_station->name . ", Mr. " . $p_member->first_name;
 
 		// TODO: check member type and render accordingly
     echo $twig->render("police_officer_dashboard.html", array(
-    	"message" => $message
+    	"message" => $message,
+    	"open_report_count" => $open_report_count
     	));
 	}
 
--- app/models/Report.php
+++ app/models/Report.php
@@ -18,4 +18,27 @@ class Report extends Base
     self::UPDATED_AT => 'integer'
     );
 
+  public static function countOpenFor($member)
+  {
+    $query = "SELECT count(*) FROM `reports`
+              WHERE `id` IN
+              ( SELECT `report_id` FROM `report_police_member_map`
+                WHERE `member_id` = {$member->id})
+              AND `status` = 'open'";
+
+    $open_report_count = 0;
+
+    if($statement = self::$db->prepare($query))
+    {
+      $statement->execute();
+      $statement->bind_result($open_report_count);
+      $statement->store_result();
+      $statement->fetch();
+      $statement->close();
+
+      return $open_report_count;
+    }
+
+  }
+
 }
