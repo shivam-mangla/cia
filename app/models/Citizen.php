bfe6eb40dc1d4345838b296878fe351a134875f6
Shivam <shivam.mangla.09@gmail.com>
2015-06-07 02:26:03 +0530

    Add models for Citizen & forwarded_CIR

---
+++ app/models/Citizen.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Models;
+
+use Models\Base;
+
+class Citizen extends Base
+{
+  protected $table = 'citizens';
+
+  protected $fillable = array('username', 'passwd', 'role', 'org_name', 'created_at', 'updated_at');
+
+
+  protected $casts = array(
+    'c_id' => 'integer',
+    'username' => 'string',
+    'passwd' => 'string',
+    'role'  => 'string', 
+    'org_name' => 'string',
+    self::CREATED_AT => 'integer',
+    self::UPDATED_AT => 'integer'
+    );
+
+  public static function findByCitizenname($citizenName)
+  {
+    $instance = new static;
+
+    return $instance->findWhere(array('username' => $citizenName));
+  }
+
+}
