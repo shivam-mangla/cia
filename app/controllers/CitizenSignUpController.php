d6ccdfd6f5018df50216b4fed9292e6df4e0669d
Shivam <shivam.mangla.09@gmail.com>
2015-06-07 15:54:15 +0530

    Add aadhaar authentication to citizen sign up

--- app/controllers/CitizenSignUpController.php
+++ app/controllers/CitizenSignUpController.php
@@ -14,7 +14,40 @@ class CitizenSignUpController
 
 	function post()
 	{
-		//TODO: do aadhaar authentication
+
+		//TODO: Write that data into db
+
+
+		// Aadhaar authentication
+		$query = array(
+			'demographics' => array(
+				'name' => array(
+					'name-value' => $_POST('name')
+				),
+				'dob' => array(
+					'dob-value' => $_POST('dob')
+				),
+				'email' => $_POST('email'),
+				'phone' => $_POST('phone')
+			)
+			);
+
+
+		// Create Http context details		
+		$contextData = array ( 
+                'method' => 'POST',
+                'header' => "Content-Type: application/json\r\n",
+                'content'=> json_encode($query) );
+
+		// Create context resource for our request
+		$context = stream_context_create (array ( 'http' => $contextData ));
+
+		// Read page rendered as result of your POST request
+		$result =  file_get_contents (
+		                  'https://ac.khoslalabs.com/hackgate/hackathon/auth/raw',  // page url
+		                  false,
+		                  $context);
+
 	}
 
 }
