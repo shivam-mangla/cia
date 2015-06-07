62c3f2dba9fddc2cc30ad182d8d2ae1cb9a5b206
Abhishek Kandoi <abhikandoi2000@gmail.com>
2015-06-07 08:36:52 +0530

    Adds method to get station id for police member

--- app/models/PoliceMember.php
+++ app/models/PoliceMember.php
@@ -28,4 +28,25 @@ class PoliceMember extends Base
     return $instance->findWhere(array('username' => $username));
   }
 
+  public function getStationId()
+  {
+    $id = $this->getKey();
+
+    $query = "SELECT `station_id` FROM `police_station_and_member_map`
+              WHERE `member_id` = {$id}";
+
+    $station_id = NULL;
+
+    if($statement = self::$db->prepare($query))
+    {
+      $statement->execute();
+      $statement->bind_result($station_id);
+      $statement->store_result();
+      $statement->fetch();
+      $statement->close();
+
+      return $station_id;
+    }
+  }
+
 }
