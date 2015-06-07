7a0f7c601d792e2e25bfec078eebb6d669d56ffb
Abhishek Kandoi <abhikandoi2000@gmail.com>
2015-06-07 08:48:45 +0530

    Adds view and controller for a single report request

---
+++ app/controllers/PoliceReportController.php
@@ -0,0 +1,35 @@
+<?php
+
+use Models\PoliceMember;
+use Models\PoliceStation;
+use Models\Report;
+
+class PoliceReportController
+{
+
+	function get($report_id)
+	{
+		$loader = new \Twig_Loader_Filesystem(__DIR__ . '/../views');
+    $twig = new \Twig_Environment($loader);
+
+    // TODO: get the user from the login cookies
+    $username = $_COOKIE["username"];
+
+		$p_member = PoliceMember::findByUsername($username);
+
+		if(NULL === $p_member)
+		{
+			header('Location: /police/login?error_msg=login_required');
+		}
+
+    $report = Report::find($report_id);
+
+		$aadhaar_no = $report->aadhaar_no;
+
+    echo $twig->render("police_report_page.html", array(
+    	"report_id" => $report_id,
+    	"aadhaar_no" => $aadhaar_no
+    	));
+	}
+
+}
